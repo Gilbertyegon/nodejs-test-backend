2a43e63ca2c85539c1e15cfa35b27f6c30c75a3c
ROBERT <robertlagat38@gmail.com>
2023-05-10 16:45:06 +0300

    first commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/users', authController.createUser);
+router.post('/signup', authController.createUser);
 router.post('/login', authController.loginUser);
+router.post('/verify', authController.verifyOtp);
+router.post('/resend', authController.resendOtp);
 
 module.exports = router;
